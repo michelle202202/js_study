474ce09ae455fe7d2c4fb7ea2b92c85f4460070f
김 은영 <michelle@Neowiz.com>
2022-06-02 19:04:14 +0900

    18주차

--- Project/app.js
+++ Project/app.js
@@ -8,24 +8,25 @@ const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저의 기본동작(새로고침)을 막아준다.
-  loginForm.classList.add(HIDDEN_CLASSNAME); // hidden 이라는 class name을 더해줘서 form 을 숨긴다.
+  loginForm.classList.add(HIDDEN_CLASSNAME); // loginForm에 hidden 이라는 class name을 더해줘서 form 을 숨긴다.
   const username = loginInput.value; // loginInput.value 값을 username 이라는 변수로 지정해주고, 그 이름을 h1에 넣는다.
   localStorage.setItem(USERNAME_KEY, username); // 그 변수 값을 localStorage에 username 이라는 key로 저장한다.
-  paintGreetings(username);
+  paintGreetings(username); // paintGreetings 함수를 호출한다. (form안에 있는 input에 입력한 유저명을 받는다.)
 }
 
 // 자주 사용하는 코드는 함수로 만들어서 사용한다.
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`; // 텍스트를 출력한다.
-  greeting.classList.remove(HIDDEN_CLASSNAME); // classList에 hidden 을 삭제한다.
+  // paintGreetings 함수는 username 이라는 인자를 받고 있고,
+  greeting.innerText = `Hello ${username}`; // 비어있는 h1 요소 안에 텍스트를 추가한다.
+  greeting.classList.remove(HIDDEN_CLASSNAME); // h1 요소로 부터 classList에 class명인 hidden 을 제거한다.
 }
 
 // localstorage에 username을 불러온다.
 const saveUsername = localStorage.getItem(USERNAME_KEY);
 
 if (saveUsername === null) {
-  // localstorage가 null 일 때
-  loginForm.classList.remove(HIDDEN_CLASSNAME); // form의 hiddin class명을 제거한다.
+  // localstorage가 null 일 때 (값이 없을 때)
+  loginForm.classList.remove(HIDDEN_CLASSNAME); // form의 hiddin class명을 제거해서 form이 화면에 표시된다.
   loginForm.addEventListener('submit', onLoginSubmit);
 } else {
   paintGreetings(saveUsername);
--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-##1주차
-각종 셋팅
+[바닐라 JS로 크롬 앱 만들기]
 
-##2주차
-문제 풀이
+##LOGIN 만들기
+  .querySelector() 사용
+  .preventDefault()의 사용
+  .addEventListener() 사용
+  class 숨기기
+  localStorag에 이름을 저장(localStorage.setItem)/불러오기(localStorage.getItem)/삭제(localStorage.removeItem)하는 api
 
-##3주차
-드림 코딩 3회 영상 시청 및 문답시간
 
-##4주차
-드림 코딩 4회 영상 시청 및 문답시간, 문제 풀이
+  의문점?
+  class를 제거(숨김?)한다는 것에 대한 개념이 잘 이해가 안감
+  코드 중 loginForm.addEventListener('submit', onLoginSubmit) 의 개념이 이해가 안감
+  html 파일에 동일한 코드를 사용했음에도 원하는 대로 실행이 안되는 현상이 있음 > source code를 복사/붙여넣기하니 들여쓰기가 다른점을 발견 > 그런데 vs code에서는 들여쓰기가 잘못된 것 처럼 나옴
