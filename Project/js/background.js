274035c76db62c066c1243ec9d0871781f6cb530
김 은영 <michelle@Neowiz.com>
2022-06-20 20:25:58 +0900

    21주차

--- Project/js/background.js
+++ Project/js/background.js
@@ -1,6 +1,26 @@
 
 // 폴더내 이미지를 파일명으로 배열화한다.
-const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"];
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
+    "14.jpg",
+    "15.jpg",
+    "16.jpg",
+    "17.jpg",
+    "18.jpg",
+];
 
 // 이미지를 랜덤으로 출력한다.
 const chosenImage = images[Math.floor(Math.random() * images.length)];
--- Project/js/todo.js
+++ Project/js/todo.js
@@ -3,24 +3,33 @@ const toDoFrom = document.getElementById("todo-from");
 const toDoInput = toDoFrom.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos"; // 자주 사용하는 string은 미리 정의해둔다.
+
+let toDos = []; 
 
 // localStorage에 저장하는 함수
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos)) // JSON.stringify() : string으로 변환해준다.
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)) // JSON.stringify() : string으로 변환해준다.
 }
 
 // 등록한 목륵을 지우는 함수
 function deleteToDo(event){
     const li = event.target.parentElement; // target은 클릭된 HTML element, parentElement는 클릭된 element의 부모
     li.remove(); // Element.remove() : 메서드는 DOM에서 요소를 제거
+
+    // .filter() : 주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 반환 // Array.prototype.filter()
+    // parseInt() : 문자열 인자를 파싱하여 특정 진수(수의 진법 체계에서 기준이 되는 값)의 정수(숫자)를 반환
+    toDos = toDos.filter((toDo => toDo.id !== parseInt(li.id))); // toDos의 배열 중에 li.id와 같은 타입이 아닌 것만 필터해서 새 배열을 만든다.
+    saveToDos(); // 새 배열을 저장한다.
 }
 
 function painToDo(newTodo){
     const li = document.createElement("li"); //li를 만든다
+    li.id = newTodo.id; // li에 id 값을 넣는다.
     
     const span = document.createElement("span"); // span을 만든다
-    span.innerText = newTodo; // span 내부에 from에서 준 새로운 텍스트(newTodo)값을 넣는다
+    span.innerText = newTodo.text; // span 내부에 from에서 준 새로운 텍스트(newTodo)값을 넣는다
+    // 오브젝트를 .text를 사용해 변환해줘야 한다.
     
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -35,9 +44,26 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; // newTodo는 toDoInput.value값을 비우기 전에 나타내는 string
     toDoInput.value = ""; // form을 submit 하면 우리는 input을 비운다.
-    toDos.push(newTodo); // newTodo를 toDos 배열에 추가한다.
-    painToDo(newTodo); // painToDo 함수에 newTodo를 넣어서 호출한다.
+    const newTodoObj = {
+        // text 값과 id 값이 포함된 newTodoObj 객체를 만든다.
+        text: newTodo,
+        id: Date.now(), // Date.now() : UTC 기준으로 1970년 1월 1일 0시 0분 0초부터 현재까지 경과된 밀리 초를 반환
+    };
+    toDos.push(newTodoObj); // newTodo를 toDos 새로운 배열에 추가한다. (과거 내역은 없는 상태)
+    painToDo(newTodoObj); // painToDo 함수에 newTodo를 넣어서 호출한다.
     saveToDos();
 }
 
 toDoFrom.addEventListener("submit", handleToDoSubmit);
+
+// 
+const savedToDos = localStorage.getItem(TODOS_KEY); // savedToDos에 TODOS_KEY 값을 가져오는 변수를 정의한다.
+
+if(savedToDos !== null){
+    // JSON.parse(text) : JSON 문자열의 구문을 분석하고, 그 결과에서 JavaScript 값이나 객체를 생성한다.
+    // text : JSON으로 변환할 문자열 
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos; // 맨 위 빈 배열에 let 을 사용하여 이전 값을 넣어 모든 값을 넣는다.
+    parsedToDos.forEach(painToDo);
+}
+
